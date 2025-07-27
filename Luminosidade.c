5562d09a58155cff682a7591eaf715aac33e635b
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2025-07-26 23:33:00 -0300

    first commit

---
+++ Luminosidade.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "display.h"
+#include "bh1750.h"
+
+#define I2C_PORT i2c0
+
+int main()
+{
+    stdio_init_all();
+    
+    i2c_inst_t *i2c = bh1750_init(i2c0);
+    bh1750_init(i2c0);  
+    display_init(); 
+    float lux;
+    char buffer[20];
+   
+
+    while (true) {
+        bh1750_read_lux(i2c, &lux);
+        sprintf(buffer, "Lum: %.2f lux", lux);
+        display_status_msg(buffer);
+        
+        sleep_ms(1000);
+    }
+}
